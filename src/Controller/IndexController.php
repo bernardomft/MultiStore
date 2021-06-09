810ce5ce877b21586fa4aa657a8b9034f0063380
bernardomft <56193745+bernardomft@users.noreply.github.com>
2021-06-09 23:48:01 +0200

    FIN

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\DesktopRepository;
+use App\Repository\LaptopRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\BrowserKit\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,17 +14,33 @@ class IndexController extends AbstractController
     /**
      * @Route("/", name="app_index")
      */
-    public function index(DesktopRepository $desktopRepository): Response
+    public function index(DesktopRepository $desktopRepository, LaptopRepository $laptopRepository): Response
     {
         $desktops = $desktopRepository->findAll();
+        $laptops = $laptopRepository->findAll();
         $products = [];
-        foreach($desktops as $d){
-            array_push($products,$d->getIdProduct());
+        $products2 = [];
+        for($i=0; $i<3;$i++){
+            array_push($products,$desktops[$i]->getIdProduct());
+            array_push($products2,$laptops[$i]->getIdProduct());
         }
+        /*foreach($desktops as $d){
+            array_push($products,$d->getIdProduct());
+        }*/
         return $this->render('index/index.html.twig', [
             'desktops' => $desktops,
-            'products' => $products
+            'productsDesktops' => $products, 
+            'laptops' => $laptops,
+            'productLaptops' => $products2
         ]);
+    }
+
+    /**
+     * @Route("/about/us", name="about")
+     */
+    public function aboutUs( ): Response
+    {
+        return $this->render('index/about.html.twig');
     }
       /**
      * @Route("/isLooged", name="app_isLooged",options={"expose"=true}, name="isLooged", methods={"POST", "GET"})
